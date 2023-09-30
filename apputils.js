73658f99a0dcf76cc77910c8ea51cf49d08423a0
annonymususer6 <annonymususer6@gmail.com>
2023-10-01 02:20:04 +0530

    final

--- apputils.js
+++ apputils.js
@@ -4,66 +4,66 @@ const winston = require('winston');
 const { combine, timestamp, printf } = winston.format;
 const DailyRotateFile = require('winston-daily-rotate-file');
 
-// const logFormat = printf(({ level, message, timestamp }) => {
-//     return `${timestamp} ${level}: ${message}`;
-// });
+const logFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+});
 
-// const logger = winston.createLogger({
-//     level: 'info',
-//     format: combine(
-//         timestamp(),
-//         logFormat
-//     ),
-//     transports: [
-//         // new winston.transports.File({ filename: process.env.LOG_DIR + "combined.log", level: 'info' }),
-//         // new winston.transports.File({ filename: process.env.LOG_DIR + "error.log", level: 'error' }),
-//         new DailyRotateFile({
-//             filename: path.join(__dirname, 'logs', 'combined-%DATE%.log'),
-//             datePattern: 'YYYY-MM',
-//             level: 'info'
-//         }),
-//         new DailyRotateFile({
-//             filename: path.join(__dirname, 'logs', 'error-%DATE%.log'),
-//             datePattern: 'YYYY-MM',
-//             level: 'error'
-//         })
-//     ]
-// });
+const logger = winston.createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        logFormat
+    ),
+    transports: [
+        // new winston.transports.File({ filename: process.env.LOG_DIR + "combined.log", level: 'info' }),
+        // new winston.transports.File({ filename: process.env.LOG_DIR + "error.log", level: 'error' }),
+        new DailyRotateFile({
+            filename: path.join(__dirname, 'logs', 'combined-%DATE%.log'),
+            datePattern: 'YYYY-MM',
+            level: 'info'
+        }),
+        new DailyRotateFile({
+            filename: path.join(__dirname, 'logs', 'error-%DATE%.log'),
+            datePattern: 'YYYY-MM',
+            level: 'error'
+        })
+    ]
+});
 
 const infoAsync = async (message) => {
-    // return new Promise((resolve, reject) => {
-    //     logger.info(message, (err) => {
-    //         if (err) {
-    //             reject(err);
-    //         } else {
-    //             resolve();
-    //         }
-    //     });
-    // });
+    return new Promise((resolve, reject) => {
+        logger.info(message, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 };
 
 const errorAsync = async (message) => {
-    // return new Promise((resolve, reject) => {
-    //     logger.error(message, (err) => {
-    //         if (err) {
-    //             reject(err);
-    //         } else {
-    //             resolve();
-    //         }
-    //     });
-    // });
+    return new Promise((resolve, reject) => {
+        logger.error(message, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 };
 
 const warnAsync = async (message) => {
-    // return new Promise((resolve, reject) => {
-    //     logger.warn(message, err => {
-    //         if (err) {
-    //             reject(err);
-    //         } else {
-    //             resolve();
-    //         }
-    //     })
-    // });
+    return new Promise((resolve, reject) => {
+        logger.warn(message, err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        })
+    });
 }
 
 function isValidAmount(amount) {
@@ -85,7 +85,7 @@ function isValidAmount(amount) {
 
 module.exports = {
     isValidAmount: isValidAmount,
-    // logger: logger,
+    logger: logger,
     infoAsync: infoAsync,
     errorAsync: errorAsync,
     warnAsync: warnAsync
